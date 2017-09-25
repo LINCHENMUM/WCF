f40c5885a07e91c08b2ecd830d5636ddd31374d4
lan chan <lan chan>
2017-09-25 10:53:35 -0500

    Add project files.

---
+++ ETBOSS.ServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ETBOSSServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ETBOSSService.ETBOSSService)))
+            {
+                host.Open();
+                Console.WriteLine("ETBOSS Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
